ead7761fc6d417ee96926319db473ee866d15d2c
kelvins213 <kelvinotilio@gmail.com>
2023-01-06 00:01:55 -0300

    dictionary application

---
+++ android/app/src/main/kotlin/com/example/dicionario/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dicionario
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:dicionario/page/dicionario.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        home: FormularioPalavra(),
+      )
+  );
+}
